a871cd845bc81ad01a94505083f5ce6407c44eab
Nikita Monastyrskyi <nikita.monastyrskyi@itomy.ch>
2019-04-04 18:09:42 +0300

    Updates

---
+++ precache-manifest.f65464c53c8309ba176323b3393f8875.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e492d2ec4487aadc7772",
+    "url": "/qr/static/js/runtime~main.e3b43f2f.js"
+  },
+  {
+    "revision": "2cd7f22da3e9968a9f77",
+    "url": "/qr/static/js/main.c3c08052.chunk.js"
+  },
+  {
+    "revision": "b63846851ff8194d8d9f",
+    "url": "/qr/static/js/2.cd382d1d.chunk.js"
+  },
+  {
+    "revision": "2cd7f22da3e9968a9f77",
+    "url": "/qr/static/css/main.1829312b.chunk.css"
+  },
+  {
+    "revision": "cef1f0498a9cbc3d8cd637d6b2759af4",
+    "url": "/qr/index.html"
+  }
+];
